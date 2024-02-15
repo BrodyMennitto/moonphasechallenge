956bdedd51642c460f39fd93af10ca003b0a3d59
Brody Mennitto <brodymennitto@Brodys-Air.ad.pcc.edu>
2024-02-15 02:05:50 -0800

    testing

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
+const NUM_IMGS = 10
+imgs = [];
+let currentImg = 0;
+
+function preload() {
+  for (let i = 0; i < NUM_IMGS; i++) {
+    imgs[i] = loadImage('/Users/brodymennitto/Documents/CC2/Project1/moonphasechallenge/.vscode/Images');
+  }
+}
+ 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1000);
+  setupButtons();
 }
 
-function draw() {
-  background(220);
+  function draw() {
+  image(imgs[currentImg], 0, 0);
+  }
+
+  const setupButtons = _ => {
+    previous = createButton('Prev');
+    previous.position(width*0.5 - 100, height + 20);
+
+    previous.mouseClicked(_ => {
+      if (currentImg > 0) currentImg --;
+     });
+
+     next = createButton('Next');
+     next.position(width*0.5 + 100, height + 20);
+
+     next.mouseClicked(_ => {
+      if (currentImg < imgs.length - 1) currentImg++;     
+    });
 }
