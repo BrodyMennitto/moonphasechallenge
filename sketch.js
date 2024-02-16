86366f54a02ad2b31b39de9fe895688de4b1d8e6
Brody Mennitto <brodymennitto@Brodys-Air.ad.pcc.edu>
2024-02-15 16:47:35 -0800

    halfway there

--- sketch.js
+++ sketch.js
@@ -1,34 +1,33 @@
-const NUM_IMGS = 10
-imgs = [];
-let currentImg = 0;
 
-function preload() {
-  for (let i = 0; i < NUM_IMGS; i++) {
-    imgs[i] = loadImage('/Users/brodymennitto/Documents/CC2/Project1/moonphasechallenge/.vscode/Images');
-  }
-}
- 
+let phases = [200, 240, 280, 300, 320, 360];
+let phase = 0;
+
 function setup() {
-  createCanvas(1000, 1000);
-  setupButtons();
+createCanvas(600, 600)
+background(0);
+fill(255);
+noStroke();
+ellipse(width/2, height/2, 100, 100);
 }
 
-  function draw() {
-  image(imgs[currentImg], 0, 0);
-  }
-
-  const setupButtons = _ => {
-    previous = createButton('Prev');
-    previous.position(width*0.5 - 100, height + 20);
-
-    previous.mouseClicked(_ => {
-      if (currentImg > 0) currentImg --;
-     });
-
-     next = createButton('Next');
-     next.position(width*0.5 + 100, height + 20);
-
-     next.mouseClicked(_ => {
-      if (currentImg < imgs.length - 1) currentImg++;     
-    });
+function draw() {
+  fill(0);
+  noStroke;
+  ellipse(phase[index], height/2, 100, 100);
 }
+
+//function 
+//index = index + 1;
+//if (index == phase.length){
+    function mouseReleased(){
+      index++;
+      index = index%6;
+    }
+
+
+
+  //fill("Yellow");
+  //textSize(32);
+  //textAlign(RIGHT);
+  //text("(" + floor(mouseX) + ", " + floor(mouseY) + ")", mouseX, mouseY);
+
