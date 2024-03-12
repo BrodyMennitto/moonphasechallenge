4cfbeeeae9d8ef7c4391b37f0393f09a3aa426f6
Brody Mennitto <brodymennitto@Brodys-Air.ad.pcc.edu>
2024-03-12 11:45:51 -0700

    it does a thing

--- sketch.js
+++ sketch.js
@@ -1,33 +1,49 @@
-
-let phases = [200, 240, 280, 300, 320, 360];
-let phase = 0;
+let phases = [200, 220, 240, 260, 280, 300, 320, 340, 360];
+let index = 0;
 
 function setup() {
-createCanvas(600, 600)
+createCanvas(600, 600);
 background(0);
+// fill(255);
+// noStroke();
+// ellipse(width/2, height/2, 100, 100);
+ }
+
+function draw() {
 fill(255);
 noStroke();
 ellipse(width/2, height/2, 100, 100);
+
+fill(0);
+noStroke(); 
+//for (i = 0; i < phases.length; i++){
+  ellipse(phases[index], height/2, 100, 100);
+  console.log(index);
+//}
+
+fill(255); 
+strokeWeight(.5);
+textAlign(CENTER);
+textSize(32);
+text('click for phase', width/2, 100);
 }
 
-function draw() {
-  fill(0);
-  noStroke;
-  ellipse(phase[index], height/2, 100, 100);
+function mouseClicked(){
+  index++;
+  if(index > phases.length){
+    index = 0;
+  }
 }
-
-//function 
 //index = index + 1;
-//if (index == phase.length){
-    function mouseReleased(){
-      index++;
-      index = index%6;
-    }
+//if (index == phase.length)
+
+//function mouseClicked()
+   
+  
+  
 
 
 
-  //fill("Yellow");
-  //textSize(32);
-  //textAlign(RIGHT);
-  //text("(" + floor(mouseX) + ", " + floor(mouseY) + ")", mouseX, mouseY);
 
+
+  
